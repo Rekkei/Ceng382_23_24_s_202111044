c29dec376b7039b515d0acc838bb7be59eb69071
Tuna Yavuz <tunayavuz.95@hotmail.com>
2024-03-20 11:58:59 +0300

    Lab5 link button added

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f476979f68b4aa25978a01d3e5d16e1aa617a4ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5683fb9b21fae4272a051eb72e9954ebbea30418")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
