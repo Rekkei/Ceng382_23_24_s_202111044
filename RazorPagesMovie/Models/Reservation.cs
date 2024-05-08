adba315c7e8437472b34fb66adac1a6b816788b6
Tuna Yavuz <tunayavuz.95@hotmail.com>
2024-05-08 16:41:45 +0300

    new branch created for lab11

--- RazorPagesMovie/Models/Reservation.cs
+++
@@ -1,8 +0,0 @@
-public class Reservation
-{
-    public int Id { get; set; }
-    public int RoomId { get; set; }  // Foreign key
-    public Room Room { get; set; }    // Navigation property
-    public DateTime DateTime { get; set; }
-    public string ReservedBy { get; set; }
-}
